4a52373736b7a1dcca1caee9cd6ce76d8bce0c46
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-08 11:55:44 +0530

    Edit main.py

--- main.py
+++ main.py
@@ -4,11 +4,21 @@ from data import question_data
 # q1 = Question(question_data[0], question_data[0])
 # print(q1.text, q1.answer)
 # print(question_data[1]["text"])
-i = 0
-question_bank = []
-for _ in question_data:
-    question = Question(question_data[i]["text"], question_data[i]["answer"])
-    question_bank.append(question)
-    i += 1
+# i = 0
+# question_bank = []
+# for question in question_data:
+#     new_question = Question(question_data[i]["text"], question_data[i]["answer"])
+#     question_bank.append(new_question)
+#     i += 1
+#
+# print(question_bank[1].text, question_bank[1]. answer)
+# print(question_bank)
 
-print(question_bank[1].text, question_bank[1]. answer)
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+print(question_bank)
