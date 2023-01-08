441f4e7d14e7e3a9e5a61e27bd9240a273f9b4e7
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-08 11:16:34 +0530

    question_bank functional

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
-class User:
-    def __init__(self, user_id, username):
-        self.user_id = user_id
-        self.username = username
-        self.followers = 0
-        self.following = 0
+from question_model import Question
+from data import question_data
 
-    def follow(self, user):
-        self.following += 1
-        user.followers += 1
+# q1 = Question(question_data[0], question_data[0])
+# print(q1.text, q1.answer)
+# print(question_data[1]["text"])
+i = 0
+question_bank = []
+for _ in question_data:
+    question = Question(question_data[i]["text"], question_data[i]["answer"])
+    question_bank.append(question)
+    i += 1
 
-
-
-
-
-user1 = User("001", "Sunny")
-user2 = User("002", "Sticky")
-user2.follow(user1)
-user1.follow(user2)
-print(user1.user_id, user1.username, user1.following, user1.followers)
-print(user2.user_id, user2.username, user2.following, user2.followers)
+print(question_bank[1].text, question_bank[1]. answer)
