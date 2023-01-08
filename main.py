1734af70bca943942da4b32224e816a7074132e3
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-08 17:38:25 +0530

    Question() functional

--- main.py
+++ main.py
@@ -1,24 +1,11 @@
 from question_model import Question
 from data import question_data
 
-# q1 = Question(question_data[0], question_data[0])
-# print(q1.text, q1.answer)
-# print(question_data[1]["text"])
-# i = 0
-# question_bank = []
-# for question in question_data:
-#     new_question = Question(question_data[i]["text"], question_data[i]["answer"])
-#     question_bank.append(new_question)
-#     i += 1
-#
-# print(question_bank[1].text, question_bank[1]. answer)
-# print(question_bank)
 
+i = 0
 question_bank = []
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
-    new_question = Question(question_text, question_answer)
+    new_question = Question(question["text"], question["answer"])
     question_bank.append(new_question)
 
-print(question_bank)
+
