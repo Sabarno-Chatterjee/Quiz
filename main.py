25a0ababf7a71ed7389bb075effa5bfc3df403f4
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-08 20:15:32 +0530

    New trivia questions added

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from quiz_brain import QuizBrain
 i = 0
 question_bank = []
 for question in question_data:
-    new_question = Question(question["text"], question["answer"])
+    new_question = Question(question["question"], question["correct_answer"])
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
@@ -15,4 +15,4 @@ while quiz.still_has_questions():
     quiz.next_question()
 
 print(f"You have completed the quiz. Your final score is: {quiz.current_score}/{quiz.question_number}")
-# print(f"{quiz.current_score}/{quiz.question_number}")
+
